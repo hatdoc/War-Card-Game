290153eef777c60bb539a5a2b52775266922bc89
Jed <hatdoc@naver.com>
2025-04-19 16:20:01 +0800

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Jed Park on 19/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ War Card GameTests/War_Card_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameTests.swift
+//  War Card GameTests
+//
+//  Created by Jed Park on 19/4/2025.
+//
+
+import Testing
+@testable import War_Card_Game
+
+struct War_Card_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

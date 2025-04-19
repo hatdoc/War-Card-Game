547f45f9897738446c8a3584bbc301f29ac769c5
Jed <hatdoc@naver.com>
2025-04-19 16:51:58 +0800

    Building UI without looking at answer

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("background-plain")
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo").padding(50)
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                
+                Image("button")
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Text("Player")
+                    Spacer()
+                    Text("CPU")
+                    Spacer()
+                }
+                .font(.title2)
+                .foregroundColor(.white)
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Text("0")
+                    Spacer()
+                    Text("0")
+                    Spacer()
+                }
+                .font(.title)
+                .foregroundColor(.white)
+                
+                Spacer()
+                Spacer()
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
